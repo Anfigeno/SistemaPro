c5e05f4b38c7de15a46b28e4a7090531ef053aef
anfigeno <dolorcriticodevastador@proton.me>
2025-05-30 22:29:21 -0500

    [Característica] Extensiones de gnome

--- modulosNixos/gnome/default.nix
+++ modulosNixos/gnome/default.nix
@@ -6,6 +6,11 @@ in {
   };
 
   config = lib.mkIf cfg.activar {
+    environment.systemPackages = with pkgs.gnomeExtensions; [
+      blur-my-shell
+      appindicator
+    ];
+
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
