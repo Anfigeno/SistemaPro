f4e2dee25b79f8380dc6f3a192140d259f6059f2
anfigeno <dolorcriticodevastador@proton.me>
2025-06-10 12:26:24 -0500

    [Característica] Atajo de yazi para cambiar a fish

--- modulosHomeManager/yazi/default.nix
+++ modulosHomeManager/yazi/default.nix
@@ -9,10 +9,19 @@ in {
     programs.yazi = {
       enable = true;
       settings = {
-        manager = {
+        mgr = {
           show_hidden = true;
           sort_dir_first = true;
         };
+      };
+      keymap = {
+        mgr.prepend_keymap = [{
+          on = "!";
+          for = "unix";
+          run = # bash
+            ''shell "${pkgs.fish}/bin/fish" --block'';
+          desc = "Abre fish aquí";
+        }];
       };
     };
   };
