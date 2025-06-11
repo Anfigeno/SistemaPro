a27f2296cb7e1a1e21ac106b18c97cc508ecf90b
anfigeno <dolorcriticodevastador@proton.me>
2025-06-11 10:53:18 -0500

    [Característica] Formateadores, configuración para astro y configuramos que se activen todos los formateadores

--- modulosHomeManager/neovim/complementos/neoformat.lua
+++ modulosHomeManager/neovim/complementos/neoformat.lua
@@ -1,6 +1,28 @@
-local formateadores_para_js = { "biome", "prettier" }
+vim.g.neoformat_run_all_formatters = 1
+
+local formateadores = {
+	biome = {
+		exe = "biome",
+		try_node_exe = 1,
+		args = { "format", '--stdin-file-path="%:p"' },
+		no_append = 1,
+		stdin = 1,
+	},
+	prettierd = {
+		exe = "prettierd",
+		args = { '"%:p"' },
+		stdin = 1,
+	},
+}
+
+-- NOTA: JavaScript
+local formateadores_para_js = { "biome" }
 vim.g.neoformat_enabled_typescript = formateadores_para_js
 vim.g.neoformat_enabled_javascript = formateadores_para_js
 vim.g.neoformat_enabled_json = formateadores_para_js
 
-vim.keymap.set('n', '<s-f>', "<cmd>Neoformat<cr>", { desc = 'Formatea el buffer' })
+-- NOTA: Astro
+vim.g.neoformat_enabled_astro = { "prettierd", "biome" }
+vim.g.neoformat_astro_prettierd = formateadores.prettierd
+vim.g.neoformat_astro_biome = formateadores.biome
+vim.keymap.set("n", "<s-f>", "<cmd>Neoformat<cr>", { desc = "Formatea el buffer" })
