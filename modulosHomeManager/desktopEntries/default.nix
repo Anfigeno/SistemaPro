7b582944aafbc0f225761bc0789275a99bfa2120
anfigeno <dolorcriticodevastador@proton.me>
2025-07-16 17:51:04 -0500

    Entrada para Neovim

--- modulosHomeManager/desktopEntries/default.nix
+++ modulosHomeManager/desktopEntries/default.nix
@@ -7,6 +7,44 @@ in {
 
   config = lib.mkIf cfg.activar {
     xdg.desktopEntries = {
+      neovim = {
+        name = "Neovim";
+        genericName = "Text Editor";
+        comment = "Edit text files with Neovim";
+        exec = "kitty -e nvim %F";
+        icon = "nvim";
+        terminal = false;
+        type = "Application";
+        categories = [ "Development" "TextEditor" ];
+        mimeType = [
+          "text/plain"
+          "text/x-makefile"
+          "text/x-c++hdr"
+          "text/x-c++src"
+          "text/x-chdr"
+          "text/x-csrc"
+          "text/x-java"
+          "text/x-moc"
+          "text/x-pascal"
+          "text/x-tcl"
+          "text/x-tex"
+          "application/x-shellscript"
+          "text/x-c"
+          "text/x-c++"
+          "text/x-python"
+          "text/x-lua"
+          "text/x-rust"
+          "text/x-go"
+          "text/x-javascript"
+          "text/x-typescript"
+          "text/html"
+          "text/css"
+          "text/x-yaml"
+          "text/x-json"
+          "text/markdown"
+          "text/x-nix"
+        ];
+      };
       floorpPerfiles = {
         name = "Floorp Perfiles";
         genericName = "Web Browser";
